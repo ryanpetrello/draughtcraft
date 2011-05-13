250658a26a5a3630e95a25351ed134bcf5617e57
Ryan Petrello <lists@ryanpetrello.com>
2011-05-12 23:12:56 -0400

    Implementing toggleable tabs.  Now when you click on a tab, the correct tab is displayed.

--- public/javascript/recipes/builder.js
+++ public/javascript/recipes/builder.js
@@ -1,17 +1,36 @@
-$.beerparts.recipes.builder.renderRecipe = function(){
+$.beerparts.recipes.builder.fetchRecipe = function(){
     $.ajax({
         url: window.location.pathname+'/async',
         cache: false,
         success: function(html){
             $("#builder").html(html)
-            $('select').selectBox({
-                'menuTransition': 'fade'
-            }).change($.Cufon);
+            $.beerparts.recipes.builder.initListeners()
             $.Cufon();
         }
     });
 };
 
+$.beerparts.recipes.builder.initTabs = function(){
+    $('.step h2 li a').click(function(e){
+
+        // Determine the index of the chosen tab
+        var index = $('.step.active h2 li a').index(this);
+        
+        // Hide all steps
+        $('.step').removeClass('active');
+
+        // Display the step at the correct index
+        $('.step').eq(index).addClass('active');
+
+        // Prevent the default <a href> behavior
+        e.preventDefault();
+    });
+};
+
+$.beerparts.recipes.builder.initListeners = function(){
+    $.beerparts.recipes.builder.initTabs();
+};
+
 $(document).ready(function(){
-    $.beerparts.recipes.builder.renderRecipe();
+    $.beerparts.recipes.builder.fetchRecipe();
 });
