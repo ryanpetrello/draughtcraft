f8b839606723e9b69a798e357c9d2401561dca9d
Ryan Petrello <lists@ryanpetrello.com>
2011-05-12 23:21:18 -0400

    Cleaning up some cruft and refactoring the tab selection Javascript.

--- public/javascript/recipes/builder.js
+++ public/javascript/recipes/builder.js
@@ -3,29 +3,45 @@ $.beerparts.recipes.builder.fetchRecipe = function(){
         url: window.location.pathname+'/async',
         cache: false,
         success: function(html){
-            $("#builder").html(html)
-            $.beerparts.recipes.builder.initListeners()
-            $.Cufon();
+            // Inject the response from the async call
+            $("#builder").html(html);
+
+            // Re-initialize event listeners
+            $.beerparts.recipes.builder.initListeners();
+
+            // Choose the "last" tab
+            $.beerparts.recipes.builder.selectTab(
+                $.beerparts.recipes.builder.currentTab 
+            );
         }
     });
 };
 
+$.beerparts.recipes.builder.currentTab = 0;
 $.beerparts.recipes.builder.initTabs = function(){
     $('.step h2 li a').click(function(e){
 
         // Determine the index of the chosen tab
         var index = $('.step.active h2 li a').index(this);
-        
-        // Hide all steps
-        $('.step').removeClass('active');
 
-        // Display the step at the correct index
-        $('.step').eq(index).addClass('active');
+        // Actually choose the tab
+        $.beerparts.recipes.builder.selectTab(index);
 
         // Prevent the default <a href> behavior
         e.preventDefault();
     });
 };
+
+$.beerparts.recipes.builder.selectTab = function(index){
+    // Hide all steps
+    $('.step').removeClass('active');
+
+    // Display the step at the correct index
+    $('.step').eq(index).addClass('active');
+
+    // Store the "current" step index for reference
+    $.beerparts.recipes.builder.currentTab = index;
+}; 
 
 $.beerparts.recipes.builder.initListeners = function(){
     $.beerparts.recipes.builder.initTabs();
