b91889b7fab5662f7212d087cea82af3171606f0
Ryan Petrello <lists@ryanpetrello.com>
2011-05-15 18:02:59 -0400

    More unit test tinkering...

---
+++ draughtcraft/tests/controllers/test_error.py
@@ -0,0 +1,9 @@
+from draughtcraft.tests     import TestApp
+
+class TestErrorHandler(TestApp):
+
+    def test_404(self):
+        response = self.get('/missing', status=404)
+        assert response.status_int == 404
+
+        assert 'Not Found' in response.body 
