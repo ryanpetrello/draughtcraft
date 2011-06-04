00501428f5c5dce350b8424c3d4a3e928313a2b8
Ryan Petrello <lists@ryanpetrello.com>
2011-06-04 00:22:12 -0400

    Refactoring CSS, and putting up a temporary placeholder for a page at /.

--- draughtcraft/controllers/root.py
+++ draughtcraft/controllers/root.py
@@ -1,7 +1,13 @@
+from pecan import expose
+
 from error      import ErrorController
 from recipes    import RecipesController
 
 class RootController(object):
+
+    @expose('index.html')
+    def index(self):
+        return dict()
 
     recipes = RecipesController()
     error   = ErrorController()
