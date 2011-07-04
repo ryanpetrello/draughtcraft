dfaa648ba4a554161ad8bafbcbc937afe3c67b84
Ryan Petrello <lists@ryanpetrello.com>
2011-07-04 12:37:42 -0400

    Unit tests for changing your password.

---
+++ draughtcraft/tests/controllers/settings/test_password.py
@@ -0,0 +1,86 @@
+from draughtcraft           import model
+from draughtcraft.tests     import TestApp, TestAuthenticatedApp
+
+
+class TestUnauthenticatedPasswordChange(TestApp):
+
+    def test_password_render(self):
+        assert self.get('/settings/password/', status=401).status_int == 401
+
+
+class TestChangePassword(TestAuthenticatedApp):
+
+    def test_password_render(self):
+        assert self.get('/settings/password/').status_int == 200
+
+    def test_missing_values(self):
+        params = {
+            'old_password'      : 'secret',
+            'password'          : 'secret2',
+            'password_confirm'  : 'secret2'
+        }
+
+        for k in params:
+            copy = params.copy()
+            del copy[k]
+            response = self.post('/settings/password/', params=copy)
+            assert 'validation_errors' in response.request.pecan
+
+        assert model.User.get(1).password == model.User.__hash_password__(
+            'secret'
+        )
+
+    def test_incorrect_old_password(self):
+        params = {
+            'old_password'      : 'wrong',
+            'password'          : 'password',
+            'password_confirm'  : 'password'
+        }
+
+        response = self.post('/settings/password/', params=params)
+        assert 'validation_errors' in response.request.pecan
+
+        assert model.User.get(1).password == model.User.__hash_password__(
+            'secret'
+        )
+
+    def test_passwords_not_match(self):
+        params = {
+            'old_password'      : 'secret',
+            'password'          : 'password',
+            'password_confirm'  : 'password2'
+        }
+
+        response = self.post('/settings/password/', params=params)
+        assert 'validation_errors' in response.request.pecan
+
+        assert model.User.get(1).password == model.User.__hash_password__(
+            'secret'
+        )
+
+    def test_new_password_length(self):
+        params = {
+            'old_password'      : 'secret',
+            'password'          : 'foo',
+            'password_confirm'  : 'foo'
+        }
+
+        response = self.post('/settings/password/', params=params)
+        assert 'validation_errors' in response.request.pecan
+
+        assert model.User.get(1).password == model.User.__hash_password__(
+            'secret'
+        )
+
+    def test_success(self):
+        params = {
+            'old_password'      : 'secret',
+            'password'          : 'newpassword',
+            'password_confirm'  : 'newpassword'
+        }
+
+        self.post('/settings/password/', params=params)
+
+        assert model.User.get(1).password == model.User.__hash_password__(
+            'newpassword'
+        )
