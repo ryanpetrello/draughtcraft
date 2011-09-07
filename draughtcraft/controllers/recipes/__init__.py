13d71d99856b1e5166e5104ce743449d2239ab0e
Ryan Petrello <lists@ryanpetrello.com>
2011-09-06 22:09:51 -0400

    More work on column sorting for the recipe browsing UI.

--- draughtcraft/controllers/recipes/__init__.py
+++ draughtcraft/controllers/recipes/__init__.py
@@ -1,5 +1,5 @@
 from pecan                                      import expose, request, abort, redirect
-from sqlalchemy                                 import or_, asc, desc
+from sqlalchemy                                 import select, and_, or_, asc, desc, func
 from draughtcraft                               import model
 from draughtcraft.lib.schemas.recipes.browse    import RecipeSearchSchema
 from create                                     import RecipeCreationController
@@ -133,11 +133,16 @@ class RecipesController(object):
         perpage = 10.0
         offset = int(perpage * (page - 1))
 
+        views = func.count(model.RecipeView.id).label('views')
+
         # map of columns
         column_map = dict(
-            type            = model.Recipe.type,
-            name            = model.Recipe.name,
-            last_updated    = model.Recipe.last_updated
+            type            = (model.Recipe.type,),
+            name            = (model.Recipe.name,),
+            author          = (model.User.last_name, model.User.first_name, model.User.username),
+            style           = (model.Style.name,),
+            last_updated    = (model.Recipe.last_updated,),
+            views           = (views,)
         )
         
         # determine the sorting direction and column
@@ -147,28 +152,64 @@ class RecipesController(object):
             DESC = desc
         ).get(direction)
 
-        query = model.Recipe.query.filter(model.Recipe.state == 'PUBLISHED')
+        where = [
+            model.Recipe.state == 'PUBLISHED'
+        ]
 
         # If applicable, filter by style
         if kw['style']:
-            query = query.filter(model.Recipe.style == kw['style'])
+            query = where.append(model.Recipe.style == kw['style'])
 
         # If applicable, filter by type (MASH, etc...)
-        query = query.filter(or_(
+        query = where.append(or_(
             model.Recipe.id == None,
             model.Recipe.type == 'MASH' if kw['mash'] else None, 
             model.Recipe.type == 'MINIMASH' if kw['minimash'] else None, 
             model.Recipe.type.in_(('EXTRACTSTEEP', 'EXTRACT')) if kw['extract'] else None, 
         ))
 
-        results = query.order_by(order_direction(order_column)).offset(offset).limit(perpage).all()
+        # Join the `recipe`, `recipeview`, `user`, and `style` tables
+        from_obj = model.Recipe.table.outerjoin(
+            model.RecipeView.table,
+            onclause = model.RecipeView.recipe_id == model.Recipe.id
+        ).outerjoin(
+            model.Style.table,
+            onclause = model.Recipe.style_id == model.Style.id
+        ).join(
+            model.User.table, 
+            onclause = model.Recipe.author_id == model.User.id
+        )
+        
+        query = select(
+            [
+                model.Recipe.id,
+                model.User.username,
+                model.Style.name,
+                views 
+            ],
+            and_(*where),
+            from_obj = [from_obj],
+            group_by = model.Recipe.id
+        )
+        total = select(
+            [func.count(model.Recipe.id)],
+            and_(*where)
+        ).execute().fetchone()[0]
+
+        recipes = model.Recipe.query.from_statement(query.order_by(
+            *[order_direction(column) for column in order_column]
+        ).offset(
+            offset
+        ).limit(
+            perpage
+        )).all()
 
         return dict(
-            pages           = int(ceil(query.count() / perpage)),
+            pages           = int(ceil(total / perpage)),
             current_page    = page,
             order_by        = order_by,
             direction       = direction,
-            recipes         = results
+            recipes         = recipes
         )
 
     create = RecipeCreationController()
