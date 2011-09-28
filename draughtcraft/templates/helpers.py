9381117e21b7690d02b81e7474b5130f717313cc
Ryan Petrello <lists@ryanpetrello.com>
2011-09-28 14:08:05 -0400

    Fixing a bug in the sort order of recipes - now they're more human-friendly.

--- draughtcraft/templates/helpers.py
+++ draughtcraft/templates/helpers.py
@@ -73,3 +73,15 @@ def format_volume(amount):
     if float(amount) == int(amount):
         return int(amount)
     return amount
+
+def alphanum_key(s):
+    """ Turn a string into a list of string and number chunks.
+        "z23a" -> ["z", 23, "a"]
+    """
+    import re
+    def tryint(s):
+        try:
+            return int(s)
+        except:
+            return s
+    return [ tryint(c) for c in re.split('([0-9]+)', s) ]
