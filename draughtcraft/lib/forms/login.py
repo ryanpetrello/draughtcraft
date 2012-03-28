4d888a14c2a46618b71ab608979b80f819a127f1
Ryan Petrello <lists@ryanpetrello.com>
2012-03-28 17:00:25 -0400

    Adding username/password validation.

--- draughtcraft/lib/forms/login.py
+++ draughtcraft/lib/forms/login.py
@@ -1,7 +1,31 @@
 from pecan.ext.wtforms import SecureForm, TextField, PasswordField, Required
+from draughtcraft import model
+from sqlalchemy import and_
 
 
 class LoginForm(SecureForm):
 
     username = TextField('Username', validators=[Required()])
     password = PasswordField('Password', validators=[Required()])
+
+    def validate(self):
+        # regular validation
+        if not super(LoginForm, self).validate():
+            return False
+
+        username, password = self.username.data, self.password.data
+        hashed = model.User.__hash_password__(password)
+
+        if not model.User.query.filter(and_(
+            model.User.username == username,
+            model.User.password == hashed
+        )).count():
+            self.username.errors.append(
+                'The provided username and password are not valid.'
+            )
+            self.password.errors.append(
+                'The provided username and password are not valid.'
+            )
+            return False
+
+        return True
