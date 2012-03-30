d54b3d6ea189a36d3cc32bd6c13a1e582972619f
Ryan Petrello <lists@ryanpetrello.com>
2012-03-30 00:09:07 -0400

    Converting the signup form to wtforms.

--- draughtcraft/controllers/signup.py
+++ draughtcraft/controllers/signup.py
@@ -1,8 +1,10 @@
 from pecan                              import expose, request, conf
 from pecan.decorators                   import after_commit
+from pecan.ext.wtforms                  import with_form
 from draughtcraft                       import model
 from draughtcraft.lib.auth              import remove_trial_recipe
 from draughtcraft.lib                   import email as emaillib
+from draughtcraft.lib.forms.signup      import SignupForm
 
 import pecan
 
@@ -21,20 +23,17 @@ def send_signup_email():
 
 class SignupController(object):
 
-    @expose(
-        generic=True,
-        template='signup/index.html'
-    )
+    @expose(generic=True, template='signup/index.html')
+    @with_form(SignupForm)
     def index(self):
         return dict()
 
-    @index.when(
-        method='POST'
-        #schema          = SignupSchema(),
-        #htmlfill        = dict(auto_insert_errors = True, prefix_error = True),
-        #error_handler   = lambda: request.path
-    )
+    @index.when(method='POST')
     @after_commit(send_signup_email)
+    @with_form(SignupForm, error_cfg={
+        'auto_insert_errors': True,
+        'handler': lambda: request.path
+    })
     def _post(self, username, password, password_confirm, email):
 
         user = model.User(
