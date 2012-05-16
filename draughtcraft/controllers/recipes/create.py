6ba7512949a096b2cf36df7c8ef4a2140dae291a
Ryan Petrello <lists@ryanpetrello.com>
2012-05-16 00:55:20 -0400

    More work on the recipe creation form.

--- draughtcraft/controllers/recipes/create.py
+++ draughtcraft/controllers/recipes/create.py
@@ -3,8 +3,9 @@ from pecan.rest                                     import RestController
 from pecan.ext.wtforms                              import with_form
 from draughtcraft                                   import model
 from draughtcraft.lib.auth                          import save_trial_recipe
-from draughtcraft.lib.units                         import to_us
+from draughtcraft.lib.units                         import to_us, to_metric
 from draughtcraft.lib.forms.recipes.create          import RecipeCreationForm
+from draughtcraft.templates.helpers                 import format_volume
 
 class RecipeCreationController(RestController):
 
@@ -19,7 +20,24 @@ class RecipeCreationController(RestController):
         if request.context['user'] is None and recipe is not None:
             redirect(recipe.url(public=False)) 
 
-        return dict()
+        form = request.pecan['form']
+        metric = request.context.get('metric') == True
+        if request.context['user']:
+            settings = request.context['user'].settings
+            volume = settings['default_recipe_volume']
+            if metric:
+                volume = round(to_metric(*(volume, 'GALLON'))[0], 3)
+            form.process(**{
+                'type': settings['default_recipe_type'],
+                'volume': format_volume(volume)
+            })
+        else:
+            form.process(**{
+                'volume': 20 if metric else 5,
+                'unit': 'LITER' if metric else 'GALLON'
+            })
+
+        return dict(form=form)
 
     @expose('json')
     @with_form(RecipeCreationForm, error_cfg={
