f4c0c9a57ae29f32d0dfd79343ee1c5c7405f61d
Ryan Petrello <lists@ryanpetrello.com>
2012-09-04 13:54:19 -0500

    More recipe builder selenium tests.

--- draughtcraft/tests/selenium/recipes/test_builder.py
+++ draughtcraft/tests/selenium/recipes/test_builder.py
@@ -1,14 +1,13 @@
 import time
 
 from selenium.webdriver.support.ui import Select
-
 from draughtcraft.tests.selenium import TestSeleniumApp
 
 
-class TestBuilder(TestSeleniumApp):
+class TestAllGrainBuilder(TestSeleniumApp):
 
     def setUp(self):
-        super(TestBuilder, self).setUp()
+        super(TestAllGrainBuilder, self).setUp()
         self.get("/")
         self.b.find_element_by_link_text("Create Your Own Recipe").click()
 
@@ -25,6 +24,33 @@ class TestBuilder(TestSeleniumApp):
 
     def blur(self):
         self.b.find_element_by_css_selector(".logo").click()
+
+    def test_defaults(self):
+        for i in range(500):
+            try:
+                b = self.b
+                self.assertEqual(
+                    "DraughtCraft - Rocky Mountain River IPA",
+                    b.title
+                )
+                self.assertEqual(
+                    "Rocky Mountain River IPA",
+                    b.find_element_by_name("name").get_attribute("value")
+                )
+                self.assertEqual(
+                    "5",
+                    b.find_element_by_name("volume").get_attribute("value")
+                )
+                assert b.find_element_by_css_selector('.step.mash') is not None
+                assert b.find_element_by_css_selector('.step.boil') is not None
+                assert b.find_element_by_css_selector('.step.ferment') \
+                    is not None
+                break
+            except:
+                pass
+            time.sleep(.01)
+        else:
+            self.fail('timed out')
 
     def test_name_change_save(self):
         self.b.find_element_by_name("name").send_keys("!")
