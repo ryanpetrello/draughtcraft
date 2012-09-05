d1b710b7043f8673688b6ead3059a4f5d4a9eef6
Ryan Petrello <lists@ryanpetrello.com>
2012-09-05 07:08:10 -0500

    Added code to make the recipe name grow as the user types.

--- public/javascript/recipes/builder.js
+++ public/javascript/recipes/builder.js
@@ -1,3 +1,12 @@
+String.prototype.toTitleCase = function () {
+    return this.replace(
+        /\w\S*/g,
+        function(txt){
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+};
+
 (function(n, $){
 
     n.recipes = n.recipes || {}, n.recipes.builder = n.recipes.builder || {};
@@ -600,6 +609,7 @@
 
                     this.recipe.initDropDowns();
                     this.recipe.dirtyFlag.reset();
+                    $('h1 input').trigger('update');
 
                 }, this))
 
@@ -655,15 +665,74 @@ $(function(){
         }
     };
 
-    String.prototype.toTitleCase = function () {
-        return this.replace(
-            /\w\S*/g,
-            function(txt){
-                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-            }
-        );
-    };
-
     // Register a JS tooltip on the author's thumbnail (if there is one).
     $('img.gravatar').tipTip({'delay': 50});
+
+    // Make the recipe name grow as the user types
+    $('h1 input').autogrow({
+        'minWidth'      : 150,
+        'maxWidth'      : 535,
+        'comfortZone'   : 25
+    });
 });
+
+(function($){
+
+    $.fn.autogrow = function(o) {
+
+        o = $.extend({
+            maxWidth: 1000,
+            minWidth: 0,
+            comfortZone: 70
+        }, o);
+
+        this.filter('input:text').each(function(){
+
+            var minWidth = o.minWidth || $(this).width(),
+                val = '',
+                input = $(this),
+                testSubject = $('<tester/>').css({
+                    position: 'absolute',
+                    top: -9999,
+                    left: -9999,
+                    width: 'auto',
+                    fontSize: input.css('fontSize'),
+                    fontFamily: input.css('fontFamily'),
+                    fontWeight: input.css('fontWeight'),
+                    letterSpacing: input.css('letterSpacing'),
+                    whiteSpace: 'nowrap'
+                }),
+                check = function() {
+
+                    if (val === (val = input.val())) {return;}
+
+                    // Enter new content into testSubject
+                    var escaped = val.replace(/&/g, '&amp;').replace(/\s/g,'&nbsp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+                    testSubject.html(escaped);
+
+                    // Calculate new width + whether to change
+                    var testerWidth = testSubject.width(),
+                        newWidth = (testerWidth + o.comfortZone) >= minWidth ? testerWidth + o.comfortZone : minWidth,
+                        currentWidth = input.width(),
+                        isValidWidthChange = (newWidth < currentWidth && newWidth >= minWidth)
+                                             || (newWidth > minWidth && newWidth < o.maxWidth);
+
+                    // Animate width
+                    if (isValidWidthChange) {
+                        input.width(newWidth);
+                    }
+
+                };
+
+            testSubject.insertAfter(input);
+
+            $(this).bind('keyup keydown blur update', check);
+            check();
+
+        });
+
+        return this;
+
+    };
+
+})(jQuery);
