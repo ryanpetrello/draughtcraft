922acafc793b3d32f625fe18cd52b2bfd59a5f96
Ryan Petrello <lists@ryanpetrello.com>
2014-06-04 23:50:44 -0400

    Fix a bug in the WSGI entrypoint.

--- ansible/wsgi.py
+++ ansible/wsgi.py
@@ -1,13 +1,14 @@
+from pecan import conf
 from pecan.deploy import deploy
 app = deploy('/opt/web/draughtcraft/src/production.py')
 
 from paste.exceptions.errormiddleware import ErrorMiddleware
 app = ErrorMiddleware(
     app,
-    error_email=app.conf.error_email,
-    from_address=app.conf.error_email,
-    smtp_server=app.conf.error_smtp_server,
-    smtp_username=app.conf.error_email,
-    smtp_password=app.conf.error_password,
+    error_email=conf.error_email,
+    from_address=conf.error_email,
+    smtp_server=conf.error_smtp_server,
+    smtp_username=conf.error_email,
+    smtp_password=conf.error_password,
     smtp_use_tls=True
 )
